1a88842e5024a75ca521f3a51fee17efa2e7cdb7
Jakub Orczyk <j.orczyk.it@gmail.com>
2024-01-26 01:39:20 +0100

    Responsywnosc

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-//paralaksa
 const parallaxContainer = document.getElementById('parallaxContainer');
 const item1 = document.getElementById('item1');
 const item2 = document.getElementById('item2');
 const item3 = document.getElementById('item3');
-
+/**
+ * Dodaje efekt paralaksy do kontenera.
+ * @param {MouseEvent} e - Obiekt zdarzenia myszy.
+ */
 parallaxContainer.addEventListener('mousemove', function (e) {
     const x = e.clientX / window.innerWidth - 0.5;
     const y = e.clientY / window.innerHeight - 0.5;
@@ -13,18 +15,38 @@ parallaxContainer.addEventListener('mousemove', function (e) {
     item3.style.transform = `translate(${x * -100}px, ${y * -100}px)`;
 });
 
-//aside
+/**
+ * Inicjalizuje nawigację boczną.
+ * @event DOMContentLoaded
+ */
 document.addEventListener('DOMContentLoaded', function () {
+    /**
+     * Linki nawigacyjne boczne.
+     * @type {NodeListOf<HTMLAnchorElement>}
+     */
     const asideLinks = document.querySelectorAll('aside a');
+    /**
+    * Sekcje boczne.
+    * @type {NodeListOf<HTMLElement>}
+    */
     const asideDivs = document.querySelectorAll('aside div');
-
+        /**
+         * Indeks aktywnego linku.
+         * @type {number}
+         */
     let activeLinkIndex = 0;
-
+        /**
+         * Ustawia aktywny link.
+         * @param {number} index - Indeks linku.
+         */
     const setActiveLink = (index) => {
         asideDivs.forEach(div => div.style.backgroundColor = '#fff');
         asideDivs[index].style.backgroundColor = '#111111';
     }
-
+        /**
+         * Obserwator intersekcji dla sekcji bocznych.
+         * @type {IntersectionObserver}
+         */
     const observer = new IntersectionObserver(entries => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
